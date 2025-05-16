d71dd850305e50d3fc0c91e31425b2397e6d48d2
Andrés Chirinos <70867410+andres-chirinos@users.noreply.github.com>
2025-05-15 20:58:54 -0400

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -10,7 +10,16 @@ st.set_page_config(
 
 st.title("📈 Peer-to-Peer Boliviano (BOB) Exchange Data Dashboard")
 st.markdown(
-    "This project contains the ETL pipeline for the Peer-to-Peer Boliviano (BOB) Exchange Data. The data is collected from various sources and transformed into a clean format for analysis. The pipeline includes data extraction, transformation, and loading processes, along with data quality checks. [Repo de Github](https://github.com/andres-chirinos/p2p-bob-exchange) [Kaggle Datasets](https://www.kaggle.com/datasets/andreschirinos/p2p-bob-exchange)"
+"""# Peer-to-Peer Boliviano (BOB) Exchange Data
+_Github Actions ETL Pipeline_
+
+This project contains the ETL pipeline for the Peer-to-Peer Boliviano (BOB) Exchange Data. The data is collected from various sources and transformed into a clean format for analysis. The pipeline includes data extraction, transformation, and loading processes, along with data quality checks.
+
+[Dataset de Kaggle](https://www.kaggle.com/datasets/andreschirinos/p2p-bob-exchange)
+[Almacenamiento RAW](https://drive.google.com/drive/u/0/folders/1q9XHo4nUIawftnjeLVfSBbEM5r9q3qjn)
+[Dashboard Interactivo](https://p2p-bob-exchange.streamlit.app/)
+[Repositorio de Github](https://github.com/andres-chirinos/p2p-bob-exchange)
+"""
 )
 
 # === Configuración de Kaggle API ===
